30fe599557578849146b2787fa8b0ac24c6c67f7
Anthony Fong <FongAnthonyM@gmail.com>
2023-12-20 11:55:32 -0800

    Added electrode label checking to exporter

--- src/xltektools/xltekucsfbids/modalities/exporters/ieegxltekbidsexporter.py
+++ src/xltektools/xltekucsfbids/modalities/exporters/ieegxltekbidsexporter.py
@@ -75,12 +75,13 @@ class IEEGXLTEKBIDSExporter(IEEGBIDSExporter, CallableMultiplexObject):
     def load_channels(self) -> list[str, ...]:
         channel_names = list(self.modality.load_electrodes()["name"])
         n_channels = len(channel_names)
-        if n_channels > 128:
-            channel_names.extend((f"BLANK{i + 1}" for i in range(n_channels, 256)))
-        else:
-            channel_names.extend((f"BLANK{i + 1}" for i in range(n_channels, 128)))
-        channel_names.extend((f"DC{i + 1}" for i in range(16)))
-        channel_names.extend(("TRIG", "OSAT", "PR", "Pleth"))
+        if n_channels < 4 or tuple(channel_names[-4:]) != ("TRIG", "OSAT", "PR", "Pleth"):
+            if n_channels > 128:
+                channel_names.extend((f"BLANK{i + 1}" for i in range(n_channels, 256)))
+            else:
+                channel_names.extend((f"BLANK{i + 1}" for i in range(n_channels, 128)))
+            channel_names.extend((f"DC{i + 1}" for i in range(16)))
+            channel_names.extend(("TRIG", "OSAT", "PR", "Pleth"))
 
         return channel_names
 
